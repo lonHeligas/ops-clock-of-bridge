18821e825bb0b6b802d8c634d0fc34c1b02f669b
Lon Heligas <lon.heligas@gmail.com>
2023-05-01 17:04:58 -0500

    trying to get the data to display properly

--- src/components/Upcomingitem.jsx
+++ src/components/Upcomingitem.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import "../styles/Upcomingitem.css"
 // import Button from '@mui/material-next/Button';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { useContext } from 'react';
 import Moment from 'react-moment';
+
 // get a date library 
 
 
 
-
-
 const Upcomingitem = (traininglesson) => {
+
+  console.log("trainingLesson is: ", traininglesson)
+  
+    JSON.stringify(traininglesson.data)      
   return (
-    <training class="training-entry">      {
-        JSON.stringify(traininglesson.data)      }
+    <training class="training-entry">    
     </training>
   )
 

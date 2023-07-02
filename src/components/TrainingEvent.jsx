d94fdd9537fce4d601f0f74f41d936d71f16ff51
Lon Heligas <lon.heligas@gmail.com>
2023-07-02 13:15:18 -0500

    working on the form

--- src/components/TrainingEvent.jsx
+++ src/components/TrainingEvent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../styles/Upcomingitem.css"
 import DatePicker from "react-datepicker"
-import { useState } from 'react';
+import { useState, onChange } from 'react';
 import Button from '@mui/material/Button';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
@@ -15,25 +15,32 @@ import setMinutes from "date-fns/setMinutes";
 
 // get the list of events like UpcomingItem and then change them to input fields
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  console.log('The submit button was clicked')
-  console.log()
 
+
+
+// const submitData = (event) => {
+//   event.preventDefault();
+//   console.log('The submit button was clicked')
+
+// }
+
+function handleChange(evt) {
+  console.log("new value", evt.target.value);
 }
 
 
 const TrainingEvent = () => {
+  instructorName = ""
   const [startDate, setStartDate] = useState(
-    setHours(setMinutes(new Date(), 30), 16)
-  );
+    setHours(setMinutes(new Date(), 30), 16)    
+  );  
     return (
-    <div> 
+    
       <FormControl>
        <p>
       <lineitem class="training-entry">
         <p>Enter a new training event here:</p>
-        <day class="lineitem day">
+        {/* <day class="lineitem day">
           Day and Time:
           <DatePicker 
           selected={startDate} 
@@ -42,31 +49,53 @@ const TrainingEvent = () => {
           dateFormat="MMMM d // h:mm aa"   
           placeholderText="Select a day and time"       
           />
-          </day>
+          </day> */}
        
-        <type class="lineitem type">
+        {/* <type class="lineitem type">
           <FormLabel id="training-row-radio-buttons-group-label">Training:</FormLabel>
-          <RadioGroup
-          row
-          aria-labelledby='training-row-radio-buttons-group-label'
-          name='row-radio-buttons-group'
-          >
-        <FormControlLabel value="radtrain" control={<Radio />} label="Radio" />
-        <FormControlLabel value="ducktrain" control={<Radio />} label="Duckling" />
-        <FormControlLabel value="other" control={<Radio />} label="Miscellaneous" />
-       
-        </RadioGroup>
-        </type>
+            <RadioGroup
+            row
+            aria-labelledby='training-row-radio-buttons-group-label'
+            name='row-radio-buttons-group'
+            >
+          <FormControlLabel 
+            value="radtrain" 
+            control={<Radio />} 
+            label="Radio" 
+            // onChange={this.onValueChange}        
+          />
+          <FormControlLabel 
+            value="ducktrain" 
+            control={<Radio />} 
+            label="Duckling" 
+            // onChange={this.onValueChange}
+          />
+          <FormControlLabel 
+            value="other" 
+            control={<Radio />} 
+            label="Miscellaneous"
+            // onChange={this.onValueChange}
+          />
+        
+          </RadioGroup>
+        </type> */}
         <instructor class="lineitem instructor">
           Instructor:
-          <input type="text" >
-          </input>
+          <input 
+            type="text"
+            name="instructorName"
+            value={this.instructorName}
+            onChange={handleChange}
+            />
+          
         </instructor>     
-        <input onClick={handleSubmit} type="submit" value="Submit" />   
+        <input type="submit" value="Submit" />   
       </lineitem>
       </p>
-      </FormControl>          
-    </div>
+      </FormControl>  
+
+
+    
   )
 }
 
